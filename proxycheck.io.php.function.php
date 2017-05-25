71fce859d2b008c288ee1663b1633252a4a59f3d
proxycheck.io <support@proxycheck.io>
2017-05-25 19:21:35 +0100

    Update proxycheck.io.php.function.php

--- proxycheck.io.php.function.php
+++ proxycheck.io.php.function.php
@@ -38,7 +38,7 @@
     
     // Performing the API query to proxycheck.io/v1/ using cURL
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $Transport_Type_String . 'proxycheck.io/v1/' . $Visitor_IP . '&key=' . $API_Key . '&vpn=' . $VPN . "&tag=" . urlencode($Query_Tag));
+    curl_setopt($ch, CURLOPT_URL, $Transport_Type_String . 'proxycheck.io/v1/' . $Visitor_IP . '&key=' . $API_Key . '&vpn=' . $VPN . '&tag=' . urlencode($Query_Tag));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $API_JSON_Result = curl_exec($ch);
     curl_close($ch);
@@ -47,7 +47,7 @@
     $Decoded_JSON = json_decode($API_JSON_Result);
 
     // Check if the IP we're testing is a proxy server
-    if ( $Decoded_JSON->proxy == "yes" && $Decoded_JSON->ip == $Visitor_IP) {
+    if ( $Decoded_JSON->proxy == "yes" && $Decoded_JSON->ip == $Visitor_IP ) {
 
       // A proxy has been detected.
       return true;
