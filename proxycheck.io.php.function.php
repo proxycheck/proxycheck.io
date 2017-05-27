7e1a6a8230bc5c37c0946a556cb74d0ba181af97
proxycheck.io <support@proxycheck.io>
2017-05-27 19:00:55 +0100

    Update proxycheck.io.php.function.php
    Changed the tag feature to use POST instead of GET as it's more reliable for special characters and won't interfere with the flags being passed to the Proxycheck.io API from the script.
    
    A custom descriptor for the tag feature has also been added which when used will override the automatic tag filing done by the script.

--- proxycheck.io.php.function.php
+++ proxycheck.io.php.function.php
@@ -15,7 +15,11 @@
     $API_Key = ""; // Supply your API key between the quotes if you have one
     $VPN = "0"; // Change this to 1 if you wish to perform VPN Checks on your visitors
     $TLS = "0"; // Change this to 1 to enable transport security, TLS is much slower though!
-    $TAG = "1"; // This setting tags your queries with a message you can view in your dashboard stats
+    $TAG = "1"; // Change this to 1 to enable tagging of your queries (will show within your dashboard)
+    
+    // If you would like to tag this traffic with a specific description place it between the quotes.
+    // Without a custom tag entered below the domain and page url will be automatically used instead.
+    $Custom_Tag = ""; // Example: $Custom_Tag = "My Forum Signup Page";
 
     // ------------------------------
     // END OF SETTINGS
@@ -29,20 +33,30 @@
     }
     
     // By default the tag used is your querying domain and the webpage being accessed
-    // However you can supply your own descriptive tag or disable tagging altogether.
-    if ( $TAG == 1 ) {
+    // However you can supply your own descriptive tag or disable tagging altogether above.
+    if ( $TAG == 1 && $Custom_Tag == "" ) {
       $Query_Tag = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+      $Post_Field = "tag=" . $Query_Tag;
+    } else if ( $TAG == 1 && $Custom_Tag != "" ) {
+      $Query_Tag = $Custom_Tag;
     } else {
       $Query_Tag = "";
     }
     
     // Performing the API query to proxycheck.io/v1/ using cURL
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $Transport_Type_String . 'proxycheck.io/v1/' . $Visitor_IP . '&key=' . $API_Key . '&vpn=' . $VPN . '&tag=' . htmlspecialchars($Query_Tag));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $ch = curl_init($Transport_Type_String . 'proxycheck.io/v1/' . $Visitor_IP . '&key=' . $API_Key . '&vpn=' . $VPN);
+    
+    $curl_options = array(
+      CURLOPT_CONNECTTIMEOUT => 30,
+      CURLOPT_POST => 1,
+      CURLOPT_POSTFIELDS => $Post_Field,
+      CURLOPT_RETURNTRANSFER => true
+    );
+    
+    curl_setopt_array($ch, $curl_options);
     $API_JSON_Result = curl_exec($ch);
     curl_close($ch);
-
+    
     // Decode the JSON from our API
     $Decoded_JSON = json_decode($API_JSON_Result);
 
